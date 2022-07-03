242544a224b901dea7a548ec7c4492b65b60344b
Aswal-lompengeng <98565917+Aswal-lompengeng@users.noreply.github.com>
2022-07-03 13:06:16 +0800

    Update _antisticker.js

--- plugins/_antisticker.js
+++ plugins/_antisticker.js
@@ -14,7 +14,7 @@ handler.before = function (m, { isAdmin, isBotAdmin }) {
       if (global.opts) {
         if (isAdmin || !isBotAdmin){		  
         }else{
-          m.reply('*Sticker detected*\nYou will in kick now..!!') // ganti text terserah kamu 
+          m.reply('*Sticker detected*\nKamu akan di kick❗..!!') // ganti text terserah kamu 
           this.groupParticipantsUpdate(m.chat, [m.sender], "remove")
         }return true
       }
