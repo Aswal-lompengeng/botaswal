7cd79bcd49e1cbcf4fba6867279b931bdc2d39fb
Aswal-lompengeng <98565917+Aswal-lompengeng@users.noreply.github.com>
2022-07-05 22:36:02 +0800

    Update smeme.js

--- plugins/smeme.js
+++ plugins/smeme.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, text, usedPrefix }) => {
   let [teks, teks2] = text.split('|')
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw 'Tidak ada foto'
+  if (!mime) throw 'Fotonya Mana Kak?🙄'
   if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} tidak support`
   let img = await q.download()
   let url = await uploadImage(img)
@@ -15,7 +15,7 @@ let handler = async (m, { conn, text, usedPrefix }) => {
   let stiker = await sticker(null, wasted, packname, author)
   conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
  } catch (e) {
-   m.reply(`Masukan format!!\nReply image dengan caption ${usedPrefix}smeme teks1|teks2\n*Jangan reply sticker*`)
+   m.reply(`Kamu salah Dek🙅‍♂️Masukan format➡️!!\nReply image dengan caption ${usedPrefix}smeme teks1|teks2\n*Jangan reply sticker*`)
   }
 }
 handler.help = ['smeme']
