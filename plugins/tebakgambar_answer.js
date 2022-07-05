4f823255d105d657d348cc5fc63dd3235d720a61
Aswal-lompengeng <98565917+Aswal-lompengeng@users.noreply.github.com>
2022-07-05 23:09:48 +0800

    Update tebakgambar_answer.js

--- plugins/tebakgambar_answer.js
+++ plugins/tebakgambar_answer.js
@@ -5,18 +5,18 @@ handler.before = async function (m) {
   let id = m.chat
   if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*hint/i.test(m.quoted.text)) return !0
   this.tebakgambar = this.tebakgambar ? this.tebakgambar : {}
-  if (!(id in this.tebakgambar)) return m.reply('Soal itu telah dijawab Nak')
+  if (!(id in this.tebakgambar)) return m.reply('Soal itu telah dijawab Nak🙅‍♂️')
   if (m.quoted.id == this.tebakgambar[id][0].id) {
     let json = JSON.parse(JSON.stringify(this.tebakgambar[id][1]))
     // m.reply(JSON.stringify(json, null, '\t'))
     if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
       global.db.data.users[m.sender].exp += this.tebakgambar[id][2]
       global.db.data.users[m.sender].tiketcoin += 1
-      m.reply(`*Benar!*\n+${this.tebakgambar[id][2]} XP\n+1 Tiketcoin`)
+      m.reply(`𝐍𝐢𝐜𝐞,𝐉𝐚𝐰𝐚𝐛𝐚𝐧𝐦𝐮 𝐁𝐞𝐧𝐚𝐫👏🏼\n+${this.tebakgambar[id][2]} XP\n+1 Tiketcoin`)
       clearTimeout(this.tebakgambar[id][3])
       delete this.tebakgambar[id]
-    } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
-    else m.reply(`*Salah!*`)
+    } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`𝐀𝐲𝐨 𝐃𝐢𝐤𝐢𝐭 𝐋𝐚𝐠𝐢🙈`)
+    else m.reply(`𝐒𝐚𝐥𝐚𝐡 𝐊𝐚𝐰𝐚𝐧☹️`)
   }
   return !0
 }
