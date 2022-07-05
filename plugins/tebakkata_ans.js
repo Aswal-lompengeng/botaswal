c6e67ff11b0172f4a456f3e4f5fc5229d04b7bf7
Aswal-lompengeng <98565917+Aswal-lompengeng@users.noreply.github.com>
2022-07-05 23:12:37 +0800

    Update tebakkata_ans.js

--- plugins/tebakkata_ans.js
+++ plugins/tebakkata_ans.js
@@ -12,11 +12,11 @@ handler.before = async function (m) {
         if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
             global.db.data.users[m.sender].exp += this.tebakkata[id][2]
             global.db.data.users[m.sender].tiketcoin += 1
-            m.reply(`*Benar!*\n+${this.tebakkata[id][2]} XP\n+1 Tiketcoin`)
+            m.reply(`𝐆𝐆 𝐁𝐄𝐍𝐀𝐑👏\n+${this.tebakkata[id][2]} XP\n+1 Tiketcoin`)
             clearTimeout(this.tebakkata[id][3])
             delete this.tebakkata[id]
-        } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
-        else m.reply(`*Salah!*`)
+        } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`𝐉𝐚𝐰𝐚𝐛𝐚𝐧𝐲𝐚 𝐦𝐞𝐧𝐝𝐞𝐤𝐚𝐭𝐢`)
+        else m.reply(`𝐒𝐚𝐥𝐚𝐡 𝐤𝐚𝐤🚫`)
     }
     return !0
 }
