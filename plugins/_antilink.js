819f0efb391f3abf8d062f6e4f4784881d9b824f
Aswal-lompengeng <98565917+Aswal-lompengeng@users.noreply.github.com>
2022-07-05 22:18:20 +0800

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -7,13 +7,13 @@ handler.before = async function (m, { user, isBotAdmin, isAdmin }) {
   let isGroupLink = linkRegex.exec(m.text)
 
   if (chat.antiLink && isGroupLink) {
-    await m.reply(`*「 ANTI LINK 」*\n\n𝐓𝐄𝐑𝐃𝐄𝐓𝐄𝐊𝐒𝐈⚠️ *${await conn.getName(m.sender)}* Mengirim Link Grup💢Aduh kak😣share link ada tempat khususnya,knp hrus disini yg jelas dilarang😑!\n\nMaaf ya,Aturan hrus dijlankan jdi kamu akn di kick!`)
+    await m.reply(`*「🚫𝘼𝙉𝙏𝙄 𝙇𝙄𝙉𝙆❗✰」*\n\n𝐓𝐄𝐑𝐃𝐄𝐓𝐄𝐊𝐒𝐈🚫 *${await conn.getName(m.sender)}* Mengirim Link Grup💢Aduh kak😣share link ada tempat khususnya,knp hrus disini yg jelas dilarang😑!\n\nMaaf ya,Aturan hrus dijlankan jdi kamu akn di kick!`)
     if (isAdmin) return m.reply('*Astaghfirullah,maaf ya min😢 kirain kamu member hehe..*')
     if (!isBotAdmin) return m.reply('*Fitur ini dpt digunakan setelah Bot menjadi Admin 🤗*')
     let linkGC = ('https://chat.whatsapp.com/' + await conn.groupInviteCode(m.chat))
     let isLinkconnGc = new RegExp(linkGC, 'i')
     let isgclink = isLinkconnGc.test(m.text)
-    if (isgclink) return m.reply('*「 ANTI LINK 」*\n\nBeliau ini kocak gaming guys,hampir aj kena kick.\nJikalau Linknya Bukan dri grup ini👀')
+    if (isgclink) return m.reply('*「 𝘼𝙉𝙏𝙄 𝙇𝙄𝙉𝙆❗ 」*\n\nAh..Beliau ini kocak gaming guys,hampir aj kena kick.\nJikalau Linknya Bukan dri grup ini👀')
     await conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
   }
   return true
