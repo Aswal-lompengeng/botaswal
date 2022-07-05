9ba0ced41017545720bbb137c8a0035363d91e4b
Aswal-lompengeng <98565917+Aswal-lompengeng@users.noreply.github.com>
2022-07-05 22:22:33 +0800

    Update afk.js

--- plugins/afk.js
+++ plugins/afk.js
@@ -3,7 +3,7 @@ let handler = async (m, { text }) => {
   user.afk = + new Date
   user.afkReason = text
   m.reply(`
-${conn.getName(m.sender)} 𝐊𝐚𝐦𝐮 𝐒𝐞𝐝𝐚𝐧𝐠 𝐀𝐅𝐊 𝐃𝐞𝐧𝐠𝐚𝐧 𝐀𝐥𝐚𝐬𝐚𝐧  ${text ? ': ' + text : ''}
+${conn.getName(m.sender)} 𝐊𝐚𝐦𝐮 𝐒𝐞𝐝𝐚𝐧𝐠 𝐀𝐅𝐊-😉-𝐃𝐞𝐧𝐠𝐚𝐧 𝐀𝐥𝐚𝐬𝐚𝐧 =  ${text ? ': ' + text : ''}
 `)
 }
 handler.help = ['afk [alasan]']
